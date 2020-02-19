667128670d8eb1f2f0e1090cc45a0cbe490a5054
Michael <mzhang@gflenv.com>
2020-02-19 15:18:04 -0500

    OAuth token setup

---
+++ Server/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class Constants
+    {
+        public const string Issuer = Audience;
+        public const string Audience = "https://localhost:44347/";
+        public const string Secret = "not_too_short_secret_otherwise_it_might_error";
+    }
+}
