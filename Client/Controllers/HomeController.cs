ad5e56a0b79cc85400cb17e414e3839b2f9c6014
Michael <mzhang@gflenv.com>
2020-02-20 15:23:55 -0500

    secure api

--- Client/Controllers/HomeController.cs
+++ Client/Controllers/HomeController.cs
@@ -4,12 +4,18 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Client.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly HttpClient _client;
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _client = httpClientFactory.CreateClient();
+        }
         public IActionResult Index()
         {
             return View();
@@ -19,6 +25,13 @@ namespace Client.Controllers
         public async Task<IActionResult> Secret()
         {
             var token = await HttpContext.GetTokenAsync("access_token");
+
+            _client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+            var serverResponse = await _client.GetAsync("https://localhost:44344/secret/index");
+
+            var apiResponse = await _client.GetAsync("https://localhost:44365/secret/index");
+
             return View();
         }
     }
--- Client/Startup.cs
+++ Client/Startup.cs
@@ -52,6 +52,9 @@ namespace Client
                         }
                     };
                 });
+
+            services.AddHttpClient();
+
             services.AddControllersWithViews()
                 .AddRazorRuntimeCompilation();
         }
